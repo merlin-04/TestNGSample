3a2a447f557fcb14df0b72e64ad807d8d519a843
merlin-04 <merlinkuriakose9@gmail.com>
2023-09-14 10:42:24 +0530

    FindElements task fixed

--- TestNGSample/src/test/java/com/obsqura/TestNGSample/FindElements.java
+++ TestNGSample/src/test/java/com/obsqura/TestNGSample/FindElements.java
@@ -7,19 +7,17 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class FindElements extends Base{
+public class FindElements extends Base {
 	@Test
-	public void sideMenuFindElements()
-	{
+	public void sideMenuFindElements() {
 		String input = "Checkbox Demo";
-		List <WebElement> menu = driver.findElements(By.xpath("//ul[contains(@class,'list-group')]//a"));
-		SoftAssert softassert = new SoftAssert();
+		List<WebElement> menu = driver.findElements(By.xpath("//ul[contains(@class,'list-group')]//a"));
 		for (WebElement menuText : menu) {
 			String actualOutput = menuText.getText();
-			softassert.assertEquals(input, actualOutput, "The actual output is not same as the " + input);
-			menuText.click();
-			break;
+			if (input.equals(actualOutput)) {
+				menuText.click();
+				break;
+			}
 		}
-//		softassert.assertAll();
 	}
 }
